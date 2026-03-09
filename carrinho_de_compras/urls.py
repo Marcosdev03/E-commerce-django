587092fb5f7781b9ba7d8ec41e5d51fb37506a15
Marcosdev03 <sdfreitas7@gmail.com>
2026-03-09 20:02:22 -0300

    subindo alterações em class adicionaraocarrinho

--- carrinho_de_compras/urls.py
+++ carrinho_de_compras/urls.py
@@ -7,9 +7,9 @@ app_name = 'carrinho_de_compras'
 urlpatterns = [
     path('', views.Carrinho.as_view(), name='carrinho'),
     path('adicionaraocarrinho/',
-         views.AdicionarProduto.as_view(), name='adicionaraocarrinho'),
+         views.AdicionarAoCarrinho.as_view(), name='adicionaraocarrinho'),
     path('removerdocarrinho/',
          views.RemoverProdutoDoCarrinho.as_view(), name='remover'),
     path('atualizar/',
-         views.AtualizarProduto.as_view(), name='atualizar'),
+         views.AtualizarProdutoDoCarrinho.as_view(), name='atualizar'),
 ]
--- carrinho_de_compras/views.py
+++ carrinho_de_compras/views.py
@@ -1,9 +1,8 @@
-from django.views import View
-from django.shortcuts import render
-from django.shortcuts import render
+from produto.models import Variacao
+from django.contrib import messages
 from django.http import HttpResponse
-from django.views.generic import ListView
-
+from django.views.generic import View
+from django.shortcuts import render, redirect, reverse, get_object_or_404
 
 class Carrinho(View):
     """
@@ -14,13 +13,37 @@ class Carrinho(View):
         return HttpResponse('Visualização do carrinho de compras')
 
 
-class AdicionarProduto(View):
+class AdicionarAoCarrinho(View):
     """
     URL: /carrinho/adicionar/<int:produto_id>/
     """
 
     def get(self, *args, **kwargs):
-        return HttpResponse('Adicionar um produto ao carrinho de compras')
+        http_referer = self.request.META.get('HTTP_REFERER', reverse(
+            'produto:lista')) 
+        
+        variacao_id = self.request.GET.get('vid')
+
+        if not variacao_id:
+            messages.error(self.request, 'Produto não encontrado')
+            return redirect(http_referer)
+        
+        variacao = get_object_or_404(Variacao, id=variacao_id)
+
+        if not self.request.session.get('carrinho'):
+            self.request.session['carrinho'] = {}
+            self.request.session.save()
+
+        carrinho = self.request.session['carrinho']
+
+        if variacao_id in carrinho:
+            # TODO: Variação existe no carrinho.
+            pass
+        else:
+            # TODO: Variação não existe no carrinho.
+            pass
+
+        return HttpResponse(f'Adicionar o produto {variacao} ao carrinho de compras')
 
 
 class RemoverProdutoDoCarrinho(View):
@@ -32,7 +55,7 @@ class RemoverProdutoDoCarrinho(View):
         return HttpResponse('Remover um produto do carrinho de compras')
 
 
-class AtualizarProduto(View):
+class AtualizarProdutoDoCarrinho(View):
     """ 
     URL: /carrinho/atualizar/<int:produto_id>/
     """
